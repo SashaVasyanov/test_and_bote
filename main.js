1cba5688ef23d42c55ed0992718ccb33812b258a
Sasha Vasyanov <primarh.alfary@yandex.ru>
2021-09-01 18:52:14 +0300

    Novo

---
+++ main.js
@@ -0,0 +1,58 @@
+// let arrOne = ['x', 'y', 'z']
+// arrOne.splice(1,1)
+// console.log(arrOne)
+
+// let arrTwo = [
+//     ['x', 'z', 'y'],
+//     ['a','b', 'n']
+// ]
+// // console.log(arrTwo)
+// console.log(arrTwo[1][2])
+
+// let arrThree = ['q', 'w', 'e']
+// arrThree.push('t')
+// console.log(arrThree)
+
+// let arrFour = ['u', 'i', 'o']
+
+// arrFour.shift()
+// console.log(arrFour)
+
+// let arrFive = ['a', 's', 'f']
+// arrFive.pop()
+// console.log(arrFive)
+
+// let arrSix = ['h', 'j', 'k']
+
+// arrSix.unshift('x')
+// console.log(arrSix)
+
+// let arrSeven = [1, 2, 3]
+// let removed = arrSeven.splice(1,1)
+// console.log(removed)
+
+// let arrEight = ['Егор', 'Егор', 'Вася', 'Коля']
+// arrEight.splice(0,2, 'Саша', 'Петя')
+// console.log(arrEight)
+
+// let arrNine = ['deda', 'vesta', 'cma', 'debil']
+// arrNine.splice(-2,1)
+// console.log(arrNine)
+
+// let arrTen = ['gor', 'tor', 'vor', 'sor', 'spor']
+// let arrEleven = arrTen.slice(1,2)
+// console.log(arrEleven)
+
+// let arrTen1 = ['1', '2', '3', '5', '6', '7']
+// let arrTwelve = arrTen1.slice()
+// console.log(arrTwelve)
+
+// let arrThirteen = ['1', 'a', 'g', 'h']
+// let arrFourteen = arrThirteen.concat('shock')
+// console.log(arrFourteen)
+
+// let arrFiveteen = ['sa', 'va', 'da', 'troi']
+// // console.log(arrFiveteen.indexOf('sa'))
+// console.log(arrFiveteen.indexOf('da', 3))
+// console.log(arrFiveteen.includes('da', 3))
+
