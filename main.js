1f447770405157e63c95a2eb1e01386c49480a98
Sasha Vasyanov <primarh.alfary@yandex.ru>
2021-09-01 21:16:39 +0300

    update1

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-// let arrOne = ['x', 'y', 'z']
-// arrOne.splice(1,1)
-// console.log(arrOne)
+let arrOne = ['x', 'y', 'z']
+arrOne.splice(1,1)
+console.log(arrOne)
 
 // let arrTwo = [
 //     ['x', 'z', 'y'],
@@ -56,3 +56,29 @@
 // console.log(arrFiveteen.indexOf('da', 3))
 // console.log(arrFiveteen.includes('da', 3))
 
+// let arrSixteen = [
+//     {name: 'Vasya', age: 14},
+//     {name: 'Deda', age: 54},
+//     {name: 'Sekunda', age: 99}
+// ]
+// // // // let result = arrSixteen.find(function(item, index, array){
+// // // //     return item.age === 99
+// // // // })
+// let result = arrSixteen.find(item => item.age === 14)
+// console.log(result)
+
+// let resultTwo = arrSixteen.findIndex(item => item.name === 'Sekunda')
+// console.log(resultTwo)
+
+//FILTER
+
+let arrEighteen = [
+    { name: 'Sasha', age: 16},
+    { name: 'Dasha', age: 17},
+    { name: 'Masha', age: 18},
+    { name: 'Pasha', age: 25},
+]
+let reuslt = arrEighteen.filter(function(item, index, array){
+    return item.age <= 18
+})
+console.log(reuslt)
