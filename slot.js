2943f86bc471e4388417550ec33b95764a70e365
Sasha Vasyanov <primarh.alfary@yandex.ru>
2021-09-05 23:51:49 +0300

    slot-machine

---
+++ slot.js
@@ -0,0 +1,26 @@
+
+// const x = Math.floor(Math.random() * 6) + 1;
+// console.log(x)
+// const z = Math.floor(Math.random() * 6) + 1;
+// console.log(z)
+
+// let c = Math.floor(Math.random() * 666) + 66;
+// c = String(c)
+// console.log(c)
+
+// const c = String(x)
+// const z = String(c)
+// const x = String(z)
+// const v = (z + x + c) 
+// const i = (v)
+// console.log(v)
+// console.log(i)  
+
+let z = Math.floor(Math.random() * 6) + 1;
+let x = Math.floor(Math.random() * 6) + 1;
+let i = Math.floor(Math.random() * 6) + 1;
+i = String(i)
+x = String(x)
+z = String(z)
+v = (z + x + i)
+console.log(v)
