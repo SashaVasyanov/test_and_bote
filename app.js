c5e9ef61233be10550090e482921951ed932ef27
Sasha Vasyanov <primarh.alfary@yandex.ru>
2021-09-09 23:58:46 +0300

    update 1.0

--- app.js
+++ app.js
@@ -1,65 +1,45 @@
-let myName = 'myName';
-let age = 22
-const header = document.querySelector('.header')
-const navLinks = document.querySelectorAll('.nav_link')
+let myName = "myName";
+let age = 22;
+const header = document.querySelector(".header");
+const navLinks = document.querySelectorAll(".nav_link");
+const btn = document.querySelector(["#testbtn"]);
 
-myName = 'Sasha'
-console.log(header)
-console.log(myName, age)
-console.log(navLinks)
+myName = "Sasha";
+console.log(header);
+console.log(myName, age);
+console.log(navLinks);
 
 function sayGoodBye() {
-    let message = 'Good Bye' + " " + myName
-    console.log(message)
+  let message = "Good Bye" + " " + myName;
+  console.log(message);
 }
-sayGoodBye()
+sayGoodBye();
 
 function simpleMath(a, b) {
-    let reuslt = a * b;
-    return reuslt
+  let reuslt = a * b;
+  return reuslt;
 }
-let multiplication = simpleMath(23, 23)
-console.log(multiplication)
+let multiplication = simpleMath(23, 23);
+console.log(multiplication);
 
+window.addEventListener("scroll", chekScroll);
 
-window.addEventListener('scroll', function() {
-   let scrollPosition = window.scrollY;
-   console.log(scrollPosition)
-   if(scrollPosition > 0) {
-    header.classList.add('red')
-   } else {
-    header.classList.remove('red')
-   }
-    
-})
+document.addEventListener("DOMContentLoaded", chekScroll);
 
+function chekScroll() {
+  let scrollPosition = window.scrollY;
+  if (scrollPosition > 0) {
+    header.classList.add("red");
+  } else {
+    header.classList.remove("red");
+  }
+}
+testbtn.addEventListener("click", function () {
+  console.log("clicked");
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+for (let navItem of navLinks) {
+  navItem.addEventListener("click", function () {
+    console.log("link clicked");
+  });
+}
