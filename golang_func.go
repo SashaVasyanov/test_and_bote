1ef920f516374dbbae3417e3655d524bb56af79c
Sasha Vasyanov <primarh.alfary@yandex.ru>
2021-09-26 20:22:27 +0300

    some go

--- golang_func.go
+++ golang_func.go
@@ -19,11 +19,18 @@ func main() {
 	fmt.Printf("%15s || %3d\n", "Her top", 9)
 	repeatLine("I am JMA", 4)
 	fmt.Print("\n")
-	paintNeed(4.2, 1.2)
-	paintNeed(5.2, 7.9)
-	paintNeed(3.3, 5.6)
 	functionD("Su", 2)
 	functionD("Su", 2)
+
+	var amount, total float64
+	amount = paintNeed(4.2, 3.0)
+	fmt.Printf("%0.2f необходимо литров краски\n", amount)
+	total += amount
+	amount = paintNeed(5.2, 3.5)
+	fmt.Printf("%0.2f необходимо литров краски", amount)
+	total += amount
+	fmt.Print("\n")
+	fmt.Printf("Всего: %0.2f литров необходимо\n", total)
 }
 
 func repeatLine(line string, repeats int) {
@@ -32,9 +39,9 @@ func repeatLine(line string, repeats int) {
 	}
 }
 
-func paintNeed(width float64, height float64) {
+func paintNeed(width float64, height float64) float64 {
 	area := width * height
-	fmt.Printf("%.2f %s \n", area/10, "Необходимое количество литров краски")
+	return area / 10.0
 }
 
 func functionD(a string, b int) {
